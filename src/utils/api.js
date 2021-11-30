205711bdbd3d8410331700b364f0b79b8c3cb411
Shannon Medcalf <shannon.medcalf@opendorse.com>
2021-11-30 10:55:47 -0600

    Updated api url after getting BE project hosted

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 axios.defaults.headers.common = { "Content-Type": "application/json" };
 
-axios.defaults.baseURL = "https://localhost:44392/api";
+axios.defaults.baseURL =
+  "https://od-app-sampleproject-ncus.azurewebsites.net/api";
