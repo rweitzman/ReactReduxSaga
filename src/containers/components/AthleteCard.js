b2db8959a98c3d3e3fa2df35c0a634ce465c1bab
Robin Weitzman <rl_weitzman@yahoo.com>
2022-02-20 15:52:54 -0800

    updated UI with material UI

--- src/containers/components/AthleteCard.js
+++ src/containers/components/AthleteCard.js
@@ -8,11 +8,27 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import CardActions from "@mui/material/CardActions";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
 import { CardActionArea } from "@mui/material";
-import { CustomCard } from "../../styles/app-styles";
+import {
+  CustomCard,
+  RedditStripe,
+  InstagramStripe,
+  AndroidStripe,
+  TwitterStripe,
+  FacebookStripe,
+  LogoBackground,
+  LogoBox,
+} from "../../styles/app-styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+//icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import RedditIcon from "@mui/icons-material/Reddit";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import AndroidIcon from "@mui/icons-material/Android";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InsertLinkIcon from "@mui/icons-material/InsertLink";
 
 export const AthleteCard = () => {
   const [updatedState, setUpdatesState] = useState([]);
@@ -43,7 +59,6 @@ export const AthleteCard = () => {
   }, [athleteInfo[0]?.imageUrl]);
 
   // make a utility function to adjust picture url so it can be displayed
-
   const convertedURL = (imageUrl) => {
     let restVar = imageUrl;
     restVar = restVar.replace("i.", "");
@@ -53,7 +68,6 @@ export const AthleteCard = () => {
 
   // Hook to  set upadated array info into a new state to render it to the user
   useEffect(() => {
-    // change this is be the uopate url with the utiltity function
     if (athleteInfo.legth !== 0) {
       athleteInfo.map((updatedItem) => {
         if (updatedItem.imageUrl) {
@@ -64,76 +78,121 @@ export const AthleteCard = () => {
     setUpdatesState(athleteInfo);
   }, [athleteInfo]);
 
+  const customCardStyles = {
+    borderRadius: "10px",
+    minHeigth: "120px",
+    maxHeigth: "120px",
+  };
+
+  const theme = createTheme({
+    typography: {
+      subtitle1: {
+        fontWeight: 900,
+        fontSize: "1.2em",
+        maxWidth: "300px",
+        overflowY: "hidden",
+        overflowX: "hidden",
+        whiteSpace: "nowrap",
+        maxHeight: "40px",
+        textOverflow: "ellipsis",
+      },
+      body1: {
+        fontWeight: 500,
+        fontSize: ".9em",
+        maxWidth: "300px",
+        overflowY: "hidden",
+        overflowX: "hidden",
+        whiteSpace: "nowrap",
+        maxHeight: "40px",
+        textOverflow: "ellipsis",
+      },
+    },
+  });
+
   const renderedList = updatedState?.map((athlete) => {
-    const { id, title, userId, content, likes, hits, categoryId, imageUrl } =
-      athlete;
+    const { id, title, hits, content, likes, categoryId, imageUrl } = athlete;
 
     return (
       <CustomCard>
         <Link to={`/allAthletes/${id}`}>
-          <Card
-            sx={{
-              borderRadius: "10px",
-              minHeigth: "120px",
-              maxHeigth: "120px",
-            }}
-            key={id}
-          >
+          <Card variant="outlined" key={id} style={{ customCardStyles }}>
             <CardActionArea>
-              <div
-                style={{
-                  display: "grid",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  // height="60"
-                  image={imageUrl}
-                  alt="green iguana"
-                  style={{
-                    width: "80px",
-                    // height: "auto",
-                    marginTop: "30px",
-                    marginBottom: "30px",
-                  }}
-                />
-              </div>
-
-              <CardContent
-                style={{
-                  backgroundColor: "#FAFAFA",
-                  borderTop: "1px solid #E3E7EC",
-                }}
-              >
-                <Box style={{ display: "flex", flexDirection: "row" }}>
-                  <Box style={{ padding: "5px" }}>
-                    <Typography
-                      gutterBottom
-                      variant="h5"
-                      component="div"
-                      style={{
-                        fontWeight: "900",
-                        fontSize: "1.1em",
-                        color: "#132640",
-                        maxWidth: "280px",
-                        overflowY: "hidden",
-                        overflowX: "hidden",
-                        whiteSpace: "nowrap",
-                        maxHeight: "20px",
-                        textOverflow: "ellipsis",
-                        border: "1px solid black"
-                      }}
-                    >
-                      {title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {userId}
-                    </Typography>
-                  </Box>
+              {categoryId === 1 && <RedditStripe />}
+              {categoryId === 2 && <InstagramStripe />}
+              {categoryId === 3 && <AndroidStripe />}
+              {categoryId === 4 && <TwitterStripe />}
+              {categoryId === 5 && <FacebookStripe />}
+              <Box direction="row" style={{ display: "flex" }}>
+                <Box style={{ width: "35%" }}>
+                  <CardMedia
+                    component="img"
+                    height="80"
+                    image={imageUrl}
+                    alt="green iguana"
+                    style={{
+                      borderRadius: "50%",
+                      width: "80px",
+                      margin: "20px",
+                    }}
+                  />
                 </Box>
-              </CardContent>
+                <Box style={{ width: "65%" }}>
+                  <CardContent>
+                    <ThemeProvider theme={theme}>
+                      <Typography
+                        gutterBottom
+                        variant="subtitle1"
+                        color="text.primary"
+                      >
+                        {title}
+                      </Typography>
+                      <Typography color="text.secondary">{content}</Typography>
+                    </ThemeProvider>
+                    <Box direction="row" style={{ display: "flex" }}>
+                      <Box
+                        direction="row"
+                        style={{ display: "flex", width: "50%" }}
+                      >
+                        <LogoBackground style={{ marginRight: "5px" }}>
+                          <LogoBox>
+                            {categoryId === 1 && (
+                              <RedditIcon style={{ color: "red" }} />
+                            )}
+                            {categoryId === 2 && (
+                              <InstagramIcon
+                                style={{ color: "rgb(228, 64, 95)" }}
+                              />
+                            )}
+                            {categoryId === 3 && (
+                              <AndroidIcon style={{ color: "black" }} />
+                            )}
+                            {categoryId === 4 && (
+                              <TwitterIcon
+                                style={{ color: "rgb(72, 120, 189)" }}
+                              />
+                            )}
+                            {categoryId === 5 && (
+                              <FacebookIcon style={{ color: "blue" }} />
+                            )}
+                          </LogoBox>
+                        </LogoBackground>
+                        {likes}
+                      </Box>
+                      <Box
+                        direction="row"
+                        style={{ display: "flex", width: "50%" }}
+                      >
+                        <LogoBackground style={{ marginRight: "5px" }}>
+                          <LogoBox>
+                            <InsertLinkIcon />
+                          </LogoBox>
+                        </LogoBackground>
+                        <Typography>{hits}</Typography>
+                      </Box>
+                    </Box>
+                  </CardContent>
+                </Box>
+              </Box>
             </CardActionArea>
           </Card>
         </Link>
@@ -155,4 +214,3 @@ AthleteCard.propTypes = {
 };
 
 export default AthleteCard;
-
--- src/styles/app-styles.js
+++ src/styles/app-styles.js
@@ -20,7 +20,6 @@ export const AppBody = styled.div`
   border-bottom-left-radius: 12px;
   box-shadow: 0 2px 9px rgba(19, 41, 63, 0.2);
   background-color: white;
-  
 `;
 
 export const AppLogo = styled.img`
@@ -34,22 +33,80 @@ export const HomeButton = styled.button`
   height: 60px;
   margin-left: 16px;
   background-color: white;
-  border: 0
+  border: 0;
 `;
 
 export const HomeHeader = styled.div`
   width: 100%;
   height: 60px;
   font-size: 3.2em;
-  display:inline-block;
-  text-align:center;
+  display: inline-block;
+  text-align: center;
   color: black;
-`
-export const CustomCard = styled.div` 
-width: 325px;
-`
+`;
+export const CustomCard = styled.div`
+  width: 325px;
+`;
 export const CustomBox = styled.div`
-display: inline-block;
-width: 100%; 
-height: 100%;
-`
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+`;
+export const RedditStripe = styled.div`
+  background-color: red;
+  width: 5px;
+  height: 186px;
+  position: absolute;
+  border-radius: 5px;
+`;
+export const InstagramStripe = styled.div`
+  background-color: rgb(228, 64, 95);
+  width: 5px;
+  height: 186px;
+  position: absolute;
+  border-radius: 5px;
+`;
+export const AndroidStripe = styled.div`
+  background-color: black;
+  width: 5px;
+  height: 186px;
+  position: absolute;
+  border-radius: 5px;
+`;
+export const TwitterStripe = styled.div`
+  background-color: rgb(72, 120, 189);
+  width: 5px;
+  height: 186px;
+  position: absolute;
+  border-radius: 5px;
+`;
+
+export const FacebookStripe = styled.div`
+  background-color: rgb(72, 120, 189);
+  width: 5px;
+  height: 186px;
+  position: absolute;
+  border-radius: 5px;
+`;
+
+export const CustomCardMedia = styled.div`
+  width: 80px;
+  margin: 20px;
+  height: 80px;
+`;
+
+export const LogoBackground = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: #eceeef;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+`;
+
+export const LogoBox = styled.div`
+  display: flex;
+  justifycontent: center;
+  alignitems: center;
+`;
